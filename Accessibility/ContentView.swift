e67d339d83ef887b30475e5245620079f2660f67
Peter Barclay <peter@peter-barclay.com>
2019-12-06 08:46:49 +0000

    Reading the value of controls - Slider Value
    Value would be read as percentage without this modifier

--- Accessibility/ContentView.swift
+++ Accessibility/ContentView.swift
@@ -5,14 +5,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var estimate = 25.0
+
     var body: some View {
-        VStack {
-            Text("Your score is")
-            Text("1000")
-                .font(.title)
-        }
-        .accessibilityElement(children: .ignore)
-        .accessibility(label: Text("Your score is 1000"))
+        Slider(value: $estimate, in: 0...50)
+            .padding()
+            .accessibility(value: Text("\(Int(estimate))"))
     }
 }
 
