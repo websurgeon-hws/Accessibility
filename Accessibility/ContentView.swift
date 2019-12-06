ba854960c02c45a52f361515576687e23aed1a57
Peter Barclay <peter@peter-barclay.com>
2019-12-06 08:43:09 +0000

    Hiding and grouping accessibility data - Combine

--- Accessibility/ContentView.swift
+++ Accessibility/ContentView.swift
@@ -6,10 +6,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image(decorative: "character")
-            .resizable()
-            .edgesIgnoringSafeArea(.all)
-            .accessibility(hidden: true)
+        VStack {
+            Text("Your score is")
+            Text("1000")
+                .font(.title)
+        }
+        .accessibilityElement(children: .combine)
     }
 }
 
